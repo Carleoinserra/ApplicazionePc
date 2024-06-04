ca51a755363bb96fa37cb46a70032cbe77792ba8
Carleoinserra <inserracarlo@gmail.com>
2024-06-04 12:20:30 +0200

    Add files via upload

---
+++ AppPc/src/main/java/com/example/demo/pcOrd.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+public class pcOrd {
+
+	
+	String modello;
+	int qnt;
+	int id;
+	public int getQnt() {
+		return qnt;
+	}
+	public void setQnt(int qnt) {
+		this.qnt = qnt;
+	}
+	public String getModello() {
+		return modello;
+	}
+	public void setModello(String modello) {
+		this.modello = modello;
+	}
+	public void setId(int int1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
